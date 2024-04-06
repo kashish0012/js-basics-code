7e71941d4eda3b8bba66c5a63cf1dbd635a0bf1f
Kashish Kumar Singh <70874028+kashish0012@users.noreply.github.com>
2024-04-06 12:11:54 +0000

    data type conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,65 @@
+let score = true;
+
+//console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+let isLoggesIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggesIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "kashish" => true
+
+
+let number = 66;
+
+let stringNumber = String(number);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *************************Operations*********************************
+
+let value = 4;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2); // addition
+// console.log(2-2); // substraction
+// console.log(2*2); // multiplication
+// console.log(2**2); // power
+// console.log(2/2); // division
+// console.log(2%2); // modulus(remainder)
+
+let str1 = "Hello"
+let str2 = " Kashish"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+let counter = gameCounter++;
+console.table([gameCounter, counter]);
